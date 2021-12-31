d0c8cc297226e365fff97b276251cbdc0557105d
Adriel Arruda <adriel.marcos.arruda@gmail.com>
2021-12-31 12:00:02 -0300

    Create class Order and Enum OrderStatus

---
+++ Enumeration/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Enumeration.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
--- Enumeration/Program.cs
+++ Enumeration/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Enumeration
+﻿using Enumeration.Entities;
+using Enumeration.Entities.Enums;
+namespace Enumeration
 {
     public static class Enumeration
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order
+            {
+                Id = 001,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
         }
     }
 }
