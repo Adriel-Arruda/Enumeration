28d1ab485eec8f5ca77acdca94577317a730adee
Adriel Arruda <adriel.marcos.arruda@gmail.com>
2021-12-31 11:43:01 -0300

    Hello world!

--- Enumeration/Program.cs
+++ Enumeration/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Enumeration
+{
+    public static class Enumeration
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
